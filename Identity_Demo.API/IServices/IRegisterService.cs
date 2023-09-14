57045c4220e7b49a782d0d6a9e307ff9b633d838
Nhâm Tuấn Đạt <139073383+datntHard@users.noreply.github.com>
2023-09-14 11:36:12 +0700

    first commit

---
+++ Identity_Demo.API/IServices/IRegisterService.cs
@@ -0,0 +1,9 @@
+﻿using Identity_Demo.Shared.Models;
+
+namespace Identity_Demo.API.IServices
+{
+    public interface IRegisterService
+    {
+        Task<Response> RegisterAsync(RegisterUser registerUser, string role);
+    }
+}
